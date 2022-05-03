af8d424324000d7850fce88dc29f38133e00ee35
Sergey <adv.sk2521@yandex.ru>
2022-05-03 17:41:20 +0300

    Added reflection, light estimation, optimization

--- Assets/Scripts/DoorOpenController.cs
+++ Assets/Scripts/DoorOpenController.cs
@@ -1,19 +1,36 @@
 using System;
 using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class DoorOpenController : MonoBehaviour, IPointerClickHandler
 {
     private Animator _animator;
+    private ARPlaneManager _planeManager;
+    private ARPointCloudManager _pointManager;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _planeManager = FindObjectOfType<ARPlaneManager>();
+        _pointManager = FindObjectOfType<ARPointCloudManager>();
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         _animator = GetComponent<Animator>();
         _animator.SetTrigger("OpenDoor");
+
+        foreach (var plane in _planeManager.trackables)
+        {
+            plane.gameObject.SetActive(false);
+        }
+        _planeManager.enabled = false;
+
+        foreach (var point in _pointManager.trackables)
+        {
+            point.gameObject.SetActive(false);
+        }
+        _pointManager.enabled = false;
     }
 }
